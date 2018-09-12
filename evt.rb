c0df46fb159a970c395a7a35df148e977a3c45ce
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2018-09-12 14:46:28 +0200

    read back

--- evt.rb
+++ evt.rb
@@ -1,4 +1,4 @@
-Dir["baric/*/*.EVT"].sort.each do | file |
+Dir["/AGILE_PROC3/FM3.119_ASDCSTDk/EVT/ag10*.EVT.gz"].sort.each do | file |
 	cmd = "./gtImporterELtelem 0 " + file.to_s
 	puts cmd
 	system cmd
